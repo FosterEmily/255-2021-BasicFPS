a3c089b7c60e1d2a661da8c48c8fd953bdbb8e93
Emily <fostere7@ferris.edu>
2021-01-20 16:31:41 -0500

    Character Movement and camera movements

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 2;
+    public float mouseSensitivityX = 10;
+    public float mouseSensitivityY = 10;
+
+    private Camera cam;
+    private CharacterController pawn;
+
+    float cameraPitch = 0;
+
+    void Start()
+    {
+
+        pawn = GetComponent<CharacterController>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    
+    void Update()
+    {
+        MovePlayer();
+        TurnPlayer();
+    }
+
+    void TurnPlayer()
+    {
+       float h = Input.GetAxis("Mouse X");
+       float v = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, h * mouseSensitivityX, 0);
+
+        // cam.transform.Rotate(v * mouseSensitivityY, 0, 0);
+
+        cameraPitch += v * mouseSensitivityY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -80, 80);
+
+        cam.transform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+    }
+
+    void MovePlayer()
+    {
+        //vectors
+        float v =  Input.GetAxis("Vertical"); //w+s / up + down: a value between -1 and 1
+        float h = Input.GetAxis("Horizontal"); //a+d /left+right : a value between -1 and 1
+
+       //  transform.position += new Vector3(moveSpeed * Time.deltaTime * h, 0, 0);
+       //  transform.position += new Vector3(0, 0, moveSpeed * Time.deltaTime * v);
+
+       // transform.position += transform.right * moveSpeed * Time.deltaTime * h;
+       //  transform.position += transfrom.forward * moveSpeed * Time.deltaTime * v;
+
+       // transform.position += (transform.right * h + transform.forward * v) * moveSpeed * Time.deltaTime;
+        Vector3 speed = (transform.right * h + transform.forward * v) * moveSpeed;
+        pawn.SimpleMove(speed);
+
+    }
+
+}
